77ff8b2f6b67a4ec35007f84ce4c45cb06d42c7a
John Franklin <jfranklin9000@gmail.com>
2020-04-30 02:37:20 -0500

    use our own Home.png instead of launch's

--- src/js/components/lib/icons/icon-home.js
+++ src/js/components/lib/icons/icon-home.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class IconHome extends Component {
   render() {
     return (
-      <img src="/~launch/img/Home.png" width={32} height={32} />
+      <img src="/~udonedit/img/Home.png" width={32} height={32} />
     );
   }
 }
